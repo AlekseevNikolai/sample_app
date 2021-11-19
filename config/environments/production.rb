3384ecb01f76a652ce4d69a5f9b59cad0d6ffee4
AlekseevNikolai <chilliman@mail.ru>
2021-11-19 21:32:03 +0300

    Change heroku host name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'fast-forest-30138.herokuapp.com'
+  host = 'shielded-sea-01532.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address              => 'smtp.sendgrid.net',
